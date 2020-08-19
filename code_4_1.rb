6415dcb726f75c3a56ea4f60e963bce7664fb59a
hiroyuki azukizawa <hiroyuki.azukizawa@nacl-med.co.jp>
2020-08-19 14:27:18 +0900

    code_4_1

--- code_4_1.rb
+++ code_4_1.rb
@@ -1,22 +1,22 @@
 #!usr/bin/ruby
 
-p "a"
+p "a-1"
 class Foo
-    p "b"
-    def initialize (a)
-        p "c"
-        @a = a
+    p "b-1"
+    def initialize (a)  #引数aを受ける初期化メソッド
+        p "b-2"
+        @a = a  #@aはインスタンス変数
     end
     def method1
-        p "d"
+        p "b-3"
         @a
     end
 end
-p "e"
-foo1 = Foo.new(1)
-p "f"
-foo2 = Foo.new(2)
-p "g"
+p "c-1"
+foo1 = Foo.new(1)   #newメソッド実行でオブジェクトを作成
+p "c-2"
+foo2 = Foo.new(2)   #newメソッド実行でオブジェクトを作成
+p "c-3"
 p foo1.method1
-p "h"
+p "c-4"
 p foo2.method1
