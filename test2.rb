228f6c81fb8601ec3b0fdafcb1cc002e028fc99f
hiroyuki azukizawa <hiroyuki.azukizawa@nacl-med.co.jp>
2020-09-11 15:15:38 +0900

    変数のテスト

--- test2.rb
+++ test2.rb
@@ -2,13 +2,10 @@
 
 p "a-1"
 class Foo
-    def self.class_name
-        self.name
-    end
     p "b-1"
     def initialize (a)  #引数aを受ける初期化メソッド
         p "b-2"
-        @@a = a  #@aはインスタンス変数
+        @@a = a  #@aはクラス変数
     end
     def self.method1
         p "b-3"
@@ -18,5 +15,5 @@ class Foo
 end
 a = Foo
 a.new(5)
-p a.class_name
+p a.class
 a.method1
