0ba0472efde42d542f353176bc0ecdf78618ce75
hiroyuki azukizawa <hiroyuki.azukizawa@nacl-med.co.jp>
2020-09-14 11:16:51 +0900

    変数のテスト

--- test2.rb
+++ test2.rb
@@ -1,23 +1,14 @@
 #!usr/bin/ruby
 
-p "a-1"
 class Foo
-    p "b-1"
     def initialize (a)  #引数aを受ける初期化メソッド
-        p "b-2"
-        @@a = a  #@aはクラス変数
+        @@a = a
     end
     def self.method1
-        p "b-3"
         @@a
         p @@a
     end
 end
 a = Foo
 a.new(5)
-b = Foo
-b.new(7)
 a.method1
-p a.object_id
-b.method1
-p b.object_id
