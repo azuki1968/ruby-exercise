f6d41b887ed0dcfa83a1c330cb4acd9ccdb0eb61
hiroyuki azukizawa <hiroyuki.azukizawa@nacl-med.co.jp>
2020-10-19 10:13:20 +0900

    templateメソッド説明追記

--- template.rb
+++ template.rb
@@ -8,6 +8,8 @@
 #（テンプレートクラスで直接呼び出した場合は例外処理を発生させる）
 #
 # 帳票作成処理を例としたサンプルコード
+
+# 抽象基底クラス（テンプレートクラス）
 class Foo
     def initialize
     end
@@ -42,9 +44,22 @@ class FooExt2 < Foo
     def method2 #帳票明細処理（サブクラス2）
     end
 end
-fooExt = FooExt1.new  #newメソッド実行でオブジェクトを作成
-fooExt.method1_thru_method3
+
+#正常処理１
+#fooExt = FooExt1.new  #newメソッド実行でオブジェクトを作成
+#fooExt.method1_thru_method3
+#実行経路 FooExt1.initialize => FooExt1.method1 => FooExt1.method2 => FooExt1.method3 
+#帳票明細処理はFooExt1（サブクラス１）で実行されるのでＯＫ
+
+#正常処理２
 #fooExt2 = FooExt2.new  #newメソッド実行でオブジェクトを作成
 #fooExt2.method1_thru_method3
+#実行経路 FooExt2.initialize => Foo.method1 => FooExt2.method2 => Foo.method3 
+#帳票明細処理はFooExt2（サブクラス２）で実行されるのでＯＫ
+
+#エラーとなる処理
 #foo = Foo.new  #newメソッド実行でオブジェクトを作成
 #foo.method1_thru_method3
+#実行経路 Foo.initialize => Foo.method1 => Foo.method2 => Foo.method3 
+#帳票明細処理がFoo（テンプレートクラス）で実行されるのでＮＧ
+#template.rb:24:in `method2': Foo method2 このメソッドが直接実行されたのでエラーとする (RuntimeError)
