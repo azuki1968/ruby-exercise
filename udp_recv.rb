bf4fa661d89221b29651b8ce317e4b4c21dd1635
hiroyuki azukizawa <hiroyuki.azukizawa@nacl-med.co.jp>
2020-12-04 11:41:10 +0900

    ruby学習(2020-12-07)

--- udp_recv.rb
+++ udp_recv.rb
@@ -1,5 +1,7 @@
 require 'socket'
 udps = UDPSocket.new()
-udps.bind("0.0.0.0", 10000)
+#bind(host, port) 定義したソケットをホストのポートに関連付ける
+udps.bind("0.0.0.0", 10000) 
+#recvでソケットからデータを引数には受け取り、文字列として返却
 p udps.recv(20000)
 udps.close
